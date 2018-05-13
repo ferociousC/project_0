e2855ed975617b69fded3eb0e153ca254b116626
magicfrog <abel.a.bodo@gmail.com>
2018-05-13 09:31:41 +0200

    Starting Garbage 34

--- Main.hx
+++ Main.hx
@@ -17,7 +17,7 @@ class Thehappenings {
 
 	static public function clientele(a:String, b:Array<Array<String>>, key:Int) {
 
-			var process_4 = new sys.io.Process('$a', b[key]);
+			var process_4 = new sys.io.Process('$a', [for(v in b[key]) v.toString()]);
 			if (process_4.exitCode() != 0) {
 			var message = process_4.stderr.readAll().toString();
 			var pos = haxe.macro.Context.currentPos();
