a724d44d360b7d5bd43349229e59c0dd6a5aeab7
magicfrog <abel.a.bodo@gmail.com>
2018-05-13 08:32:29 +0200

    Starting Garbage 10

--- Main.hx
+++ Main.hx
@@ -14,6 +14,18 @@ class Thehappenings {
 	  	}
 	  	return chr0n;
 	} 
+
+	static public function clientele(a:String, b:Array<String>) {
+			var process_4 = new sys.io.Process('$a', ['$b']);
+			if (process_4.exitCode() != 0) {
+			var message = process_4.stderr.readAll().toString();
+			var pos = haxe.macro.Context.currentPos();
+			haxe.macro.Context.error("Cannot execute process_4 . " + message, pos);
+			};
+			var commitHash_4 = process_4.stdout.readAll();
+			trace("Warning: " + commitHash_4);
+
+	}
 }
 
 
@@ -50,14 +62,8 @@ class Main {
 	var commitHash_3 = process_3.stdout.readAll();
 	trace("Warning: " + commitHash_3);
 
-	var process_4 = new sys.io.Process('git', ['push', '-u', 'origin', 'development']);
-		if (process_4.exitCode() != 0) {
-		var message = process_4.stderr.readAll().toString();
-		var pos = haxe.macro.Context.currentPos();
-		haxe.macro.Context.error("Cannot execute process_4 . " + message, pos);
-	};
-	var commitHash_4 = process_4.stdout.readAll();
-	trace("Warning: " + commitHash_4);
+	var command_4 = new Array(); command_4.push('push'); command_4.push('origin'); command_4.push('master');
+	Thehappenings.clientele('git',command_4);
 
    }
    	
