55de0221ce9162429488a2331b6da8d89eecf70f
magicfrog <abel.a.bodo@gmail.com>
2018-05-13 07:41:27 +0200

    Starting Garbage 5... i gotta make something to rewrite this number

--- Main.hx
+++ Main.hx
@@ -1,5 +1,25 @@
+class Thehappenings {
+	static public function gitcoal(props:Any) {
+
+	} 
+}
+
+
 class Main {
   static public function main():Void {
+	var quant:String = './chronicl.dt';
+  	var i = 5;
+  	var kyno:String = sys.io.File.getContent(quant); 
+  	if (kyno != '') {
+  	var chronicl = sys.io.File.getContent(quant);
+  	var chron = Std.parseInt(chronicl);
+  	chron++;
+  	chronicl = Std.string(chron);
+  	sys.io.File.saveContent(quant, chronicl);
+ 	} else {
+  	var chronicl = sys.io.File.append ('1');
+  	}
+
 	var process_0 = new sys.io.Process('git', ['status']);
 	if (process_0.exitCode() != 0) {
 	  var message = process_0.stderr.readAll().toString();
@@ -13,7 +33,7 @@ class Main {
    var commitHash_2 = process_2.stdout.readAll();
    trace("Warning: " + commitHash_2);
 
-	var process_3 = new sys.io.Process('git', ['commit', '-am', 'Starting Garbage 3... i gotta make something to rewrite this number']);
+	var process_3 = new sys.io.Process('git', ['commit', '-am', 'Starting Garbage 5... i gotta make something to rewrite this number']);
 	if (process_3.exitCode() != 0) {
 	var message = process_3.stderr.readAll().toString();
 	var pos = haxe.macro.Context.currentPos();
@@ -21,15 +41,6 @@ class Main {
 	};
 	var commitHash_3 = process_3.stdout.readAll();
 	trace("Warning: " + commitHash_3);
-
-	var process_4 = new sys.io.Process('git', ['push', '-u', 'origin', 'development']);
-		if (process_4.exitCode() != 0) {
-		var message = process_4.stderr.readAll().toString();
-		var pos = haxe.macro.Context.currentPos();
-		haxe.macro.Context.error("Cannot execute process_4 . " + message, pos);
-	};
-	var commitHash_4 = process_4.stdout.readAll();
-	trace("Warning: " + commitHash_4);
 
    }
    	
--- garbage.hx
+++ garbage.hx
@@ -8,3 +8,11 @@
 	var commitHash_1:String = process_1.stdout.readLine();
 	trace("Warning: " + commitHash_1);
 	
+	var process_4 = new sys.io.Process('git', ['push', '-u', 'origin', 'development']);
+		if (process_4.exitCode() != 0) {
+		var message = process_4.stderr.readAll().toString();
+		var pos = haxe.macro.Context.currentPos();
+		haxe.macro.Context.error("Cannot execute process_4 . " + message, pos);
+	};
+	var commitHash_4 = process_4.stdout.readAll();
+	trace("Warning: " + commitHash_4);
