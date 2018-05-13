ac9c32325908399b5d87ff72e44788359b8061da
magicfrog <abel.a.bodo@gmail.com>
2018-05-13 07:06:42 +0200

    Starting Garbage 3... i gotta make something to rewrite this number

---
+++ garbage.hx
@@ -0,0 +1,10 @@
+
+   var process_1 = new sys.io.Process('git', ['checkout', 'development']);
+   if (process_1.exitCode() != 0) {
+	  var message = process_1.stderr.readAll().toString();
+	  var pos = haxe.macro.Context.currentPos();
+	  haxe.macro.Context.error("Cannot execute process_1. " + message, pos);
+	};
+	var commitHash_1:String = process_1.stdout.readLine();
+	trace("Warning: " + commitHash_1);
+	
