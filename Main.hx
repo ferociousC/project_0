697e3fc5c977453b839412886cd7022d4e93e53e
magicfrog <abel.a.bodo@gmail.com>
2018-05-13 10:31:44 +0200

    Starting Garbage 71

--- Main.hx
+++ Main.hx
@@ -11,7 +11,7 @@ class Main {
   	var command_1 = ['add', '*'];
   	var command_2 = ['commit', '-am', 'Starting Garbage $chron'];
   	var command_3 = ['push', 'origin', 'development'];
-	var command_5 = ['checkout', 'master'];
+	var command_5 = ['checkout', 'origin', 'master'];
 	var command_4 = ['merge', 'development'];
 
 	clientele('git', command_0);
